05e45a456e681ce90db3721f215fc1ebed014b06
nourchabchoub <n.chabchoub@student.utwente.nl>
2023-12-09 18:29:20 +0100

    finished claim page

--- web/src/pages/claim.js
+++ web/src/pages/claim.js
@@ -4,20 +4,38 @@ import "./styles/claim.css"
 export default class Claim extends Component{
   constructor() {
     super();
-    this.state = { 
-      input : null
+    this.state = {
+      show: false,
+      input: ""
     }
   }
 
+
   render() {
     return(
-      <div>
-        <h1 className="claimText"> Claim </h1>
-        <h2 className="writeYourInfoHereText"> Write your info here: </h2>
-        <input placeholder="Type here" onChange={(e) => {this.setState({input : e.target.value})}}/>
-        <button onClick={()=>{console.log("Hiiiiii")}}> Subscribe </button>
-        <p>{this.state.input}</p>
+    <div className="center">
+      <div className="card">
+        <h1 className=""> Claim </h1>
+        <div className="claimContainer">
+          <input className="truc" placeholder="Type here ..." onChange={(e) => {this.setState({...this.state, input: e.target.value})}} />
+
+        </div>
+        <button className="centerButton" onClick={()=> {this.setState({...this.state, show:true})}}> Query </button>
       </div>
+      {
+        this.state.show == true ? 
+        <div className="card truc2"style={{marginLeft: '20px'}}>
+            <div><h2>Results</h2><p>Claim this amount of ETH</p></div>
+            <button className="centerButton" onClick={()=> {}}> Claim </button>
+
+
+        </div>
+        :null
+      }
+    </div>
     );
+
   };
-}
+
+
+};
